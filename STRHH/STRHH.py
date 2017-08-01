6839dc991b7bc24faa9e70f2709a688eb0018007
Chris Parjaszewski ▮ Skygate <chris.parjaszewski@skygate.io>
2017-08-01 12:04:10 +0200

    fix for STRHH, quick solution for VSR

--- STRHH/STRHH.py
+++ STRHH/STRHH.py
@@ -2,8 +2,8 @@ import sys
 
 t = int(input())
 while t > 0:
-    t -= 1
-    line = sys.stdin.readline()
-    halfline = line[:len(line)/2]
-    secondhalfline = halfline[::2]
-    print(secondhalfline)
+    t = t - 1
+    line = sys.stdin.readline().strip()
+    half_line = line[:len(line)/2]
+    even_half_line = half_line[0:len(half_line):2]
+    print(even_half_line)
